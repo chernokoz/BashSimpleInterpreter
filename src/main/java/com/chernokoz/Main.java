85fa92414794f3720204c5455b87757efea8e61c
Bobyshev Nikita <Gammanik@gmail.com>
2020-09-10 12:54:44 +0300

    fix import error

--- src/main/java/com/chernokoz/Main.java
+++ src/main/java/com/chernokoz/Main.java
@@ -1,7 +1,6 @@
 package com.chernokoz;
 
 import com.chernokoz.commands.Command;
-import com.chernokoz.exceptions.CommandNotFoundException;
 import com.chernokoz.commands.CommandPwd;
 import com.chernokoz.exceptions.ExitException;
 import com.chernokoz.exceptions.StopException;
